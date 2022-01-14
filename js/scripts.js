618db2ef16b0837d3b747bdf0090a35b7534eae9
Alex Adamovic <alexanderadamovic@gmail.com>
2022-01-14 12:03:56 -0800

    finish branching in scripts.js to return different results from form

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,16 @@ $(document).ready(function(){
     const millionaire = $("select#millionaire").val();
     const licks = parseInt($("input#licks").val());
 
-    if (numberCombo < 1000000) {
+    if (numberCombo <= 1000000) {
       $(".language-result-circus").show();
+    } else if (licks >= 1000 ) {
+      $(".language-result-broke").show();
+    } else if (millionaire === "fifty" ) {
+      $(".language-result-java").show();
+    } else if (trueFalseCool === "alsoTrueCool" && millionaire === "phone") {
+      $(".language-result-ruby").show();
+    } else {
+      $(".language-result-python").show();
     }
   
   });
